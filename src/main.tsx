ab8c75ebe5e36e416474dd32c29063d5a61d2eef
Parchello <parchellos@gmail.com>
2024-05-23 19:07:10 +0300

    more depend add

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createTheme, ThemeProvider } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const theme = createTheme({
+  typography: {
+    fontFamily: "Courier New, serif",
+    allVariants: { color: "bisque" },
+  },
+});
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
