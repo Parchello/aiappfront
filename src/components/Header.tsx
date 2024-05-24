aa753c4f8e5ff9f5d05b509ddba03623a1948804
Parchello <parchellos@gmail.com>
2024-05-24 13:43:49 +0300

    header start

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,49 @@
 import { AppBar, Toolbar } from "@mui/material";
 import Logo from "./shared/Logo/Logo.tsx";
 import React from "react";
+import { useAuth } from "../context/AuthContext.tsx";
+import NaivgationLink from "./shared/NavigationLink/NavigationLink.tsx";
 
 const Header = () => {
+  const auth = useAuth();
   return (
     <AppBar sx={{ bgcolor: "transparent", position: "static" }}>
       <Toolbar sx={{ display: "flex" }}>
         <Logo />
+        <div>
+          {auth?.isLoggedIn ? (
+            <>
+              <NaivgationLink
+                bg="#00fffc"
+                to="/chat"
+                text="Go to chat"
+                textColor="black"
+              />
+              <NaivgationLink
+                bg="51538f"
+                to="/"
+                text="Logout"
+                textColor="white"
+                onCklick={auth.logout}
+              />
+            </>
+          ) : (
+            <>
+              <NaivgationLink
+                bg="#00fffc"
+                to="/login"
+                text="Login"
+                textColor="black"
+              />
+              <NaivgationLink
+                bg="51538f"
+                to="/signup"
+                text="Signup"
+                textColor="white"
+              />
+            </>
+          )}
+        </div>
       </Toolbar>
     </AppBar>
   );
